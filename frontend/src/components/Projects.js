988155671484cba9ea92206336a93fb25708bb6e
Juhun Park <riann3207@gmail.com>
2024-12-02 13:39:17 -0500

    projects

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -5,47 +5,14 @@ function Projects() {
   return (
     <section className="projects" id="projects">
       <p className="project__title">{">"} Projects</p>
-      <div className="projects__container">
-        <div className="project">
-          <h3>Project 1</h3>
-          <p>
-            A brief description of the project. What technology did you use?
-            What problem does it solve?
-          </p>
-          <a href="#" target="_blank">
-            View Project
-          </a>
-        </div>
-        <div className="project">
-          <h3>Project 2</h3>
-          <p>
-            A brief description of the project. What technology did you use?
-            What problem does it solve?
-          </p>
-          <a href="#" target="_blank">
-            View Project
-          </a>
-        </div>
-        <div className="project">
-          <h3>Project 3</h3>
-          <p>
-            A brief description of the project. What technology did you use?
-            What problem does it solve?
-          </p>
-          <a href="#" target="_blank">
-            View Project
-          </a>
-        </div>
-        <div className="project">
-          <h3>Project 4</h3>
-          <p>
-            A brief description of the project. What technology did you use?
-            What problem does it solve?
-          </p>
-          <a href="#" target="_blank">
-            View Project
-          </a>
-        </div>
+      <div class="projects__container">
+        <div class="project">Project 1</div>
+        <div class="project">Project 2</div>
+        <div class="project">Project 3</div>
+        <div class="project">Project 4</div>
+        <div class="project">Project 5</div>
+        <div class="project">Project 6</div>
+        <div class="project">Project 7</div>
       </div>
     </section>
   );
